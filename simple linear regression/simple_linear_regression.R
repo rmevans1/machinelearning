fdc808151d51c522ad4eb04a3800687ec2883ce5
Robert Evans <robertevans@robertevansmb.com>
2017-02-21 14:15:09 -0500

    Process data for simple linear regression R

---
+++ simple linear regression/simple_linear_regression.R
@@ -0,0 +1,17 @@
+# Simple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('Salary_Data.csv')
+# dataset = dataset[, 2:3]
+
+# Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
